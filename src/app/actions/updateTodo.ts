b8ecea328096b78e87d304bffee510cc3bda1cfb
demola malomo <malomo.alaba@yahoo.com>
2023-12-27 23:54:32 +0100

    typo fix

--- src/app/actions/updateTodo.ts
+++ src/app/actions/updateTodo.ts
@@ -9,16 +9,13 @@ export const updateTodo = async (_: FormResponseType, formData: FormData): Promi
     const description = String(formData.get('description'));
     const id = String(formData.get('id'));
 
-    const response = await xata.db.Todo.update(id, { description })
-
-    console.log(response);
-
+    const response = await xata.db.Todo.update(id, { description });
 
     if (response?.description) {
         redirect('/');
         return {
             type: 'success',
-            message: 'Todo deleted successfully!',
+            message: 'Todo updated successfully!',
         };
     } else {
         return {
